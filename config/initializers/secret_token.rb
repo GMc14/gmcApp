6b55f0cf58c0c21f9eed41902595e87834d2b4be
GMc <git@gmc14.com>
2010-12-12 04:56:52 -0800

    1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GmcApp::Application.config.secret_token = 'c342f2fad278f0aa65f8f325c218e92d374dd02d8626a65187f575c8e69d152a209420c6e6cdde78debb9ce57aa4f873d8620f6915d8724d34cbc3d079db9d2e'
